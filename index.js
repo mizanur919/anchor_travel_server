de58155d8f96d7661d78a9262f8f2355a414063a
mizan <mizanur919@gmail.com>
2022-01-27 22:58:20 +0600

    inita

---
+++ index.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Configuration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4b6iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("AnchorTravelAgency");
+
+    // Collections
+    const reviewsCollection = database.collection("reviews");
+    const blogCollection = database.collection("blogs");
+
+    ///////////////////////////
+    //// Review Collection ////
+    ///////////////////////////
+
+    // GET API
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // POST API
+    app.post("/reviews", async (req, res) => {
+      const newReview = req.body;
+      const cursor = await reviewsCollection.insertOne(newReview);
+      res.send(cursor);
+    });
+
+    // DELETE API
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // GET Single Data API
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const review = await reviewsCollection.findOne(query);
+      res.json(review);
+    });
+
+    ///////////////////////////
+    //// Blog Collection ////
+    ///////////////////////////
+
+    // GET API
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find({});
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
+
+    // POST API
+    app.post("/blogs", async (req, res) => {
+      const newBlog = req.body;
+      console.log(newBlog);
+      const cursor = await blogCollection.insertOne(newBlog);
+      res.send(cursor);
+    });
+
+    // DELETE API
+    app.delete("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await blogCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // GET Single Data API
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const blog = await blogCollection.findOne(query);
+      res.json(blog);
+    });
+
+    ///////////////////////
+    ///// Cart ///////////
+    ///////////////////////
+
+    // Load Cart Data According To User ID
+    app.get("/cart/:uid", async (req, res) => {
+      const uid = req.params.uid;
+      const query = { uid: uid };
+      const result = await cartCollection.find(query).toArray();
+      res.json(result);
+    });
+
+    // GET Cart Data
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // Add Data to cart collection
+    app.post("/cart/add", async (req, res) => {
+      const service = req.body;
+      const result = await cartCollection.insertOne(service);
+      res.json(result);
+    });
+
+    // DELETE From cart
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server from node server");
+});
+
+app.listen(port, () => {
+  console.log("listing form port ", port);
+});
